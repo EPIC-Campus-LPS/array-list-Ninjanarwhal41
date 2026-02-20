9574485d4beac880c286b498671320d7565f310c
Ninjanarwhal41 <elicohrs@gmail.com>
2026-02-20 15:06:22 -0700

    worked on leaderboard class

--- src/Leaderboard.java
+++ src/Leaderboard.java
@@ -11,22 +11,63 @@ public class Leaderboard {
 
     public void addScore(String name, int score){
         Score e = new Score(name, score);
-        scoreboard.add(e);
+        for (int i = 0; i < capacity; i++){
+            if (){
+
+            }
+        }
     }
 
     public int getHighScore(){
-        return 0;
+        return scoreboard.getLast().getScore();
     }
 
     public int getAverageScore(){
-        return 0;
+        int average = 0;
+        for (Score s : scoreboard){
+            average += s.getScore();
+        }
+        return average / capacity;
     }
 
     public boolean break1000(){
-        return false;
+        return scoreboard.getLast().getScore() > 1000;
+        //since the last element will always be the highest
     }
 
     public void printLeaderboard(){
-        System.out.println("Placeholder");
+        for (int i = capacity; i > 0; i--){
+            System.out.println(scoreboard);
+        }
+    }
+
+    public String toString(){
+        for ( int i = 0; i < capacity; i++){
+            System.out.println("next time!!");
+        }
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Leaderboard board = new Leaderboard(5);
+
+        board.addScore("ABC", 100);
+        board.addScore("DEF", 200);
+        board.addScore("GHI", 250);
+        board.addScore("JKL", 300);
+        board.addScore("MNO", 205);
+
+        System.out.println("High Score: " + board.getHighScore());
+        System.out.println("Average Score: " + board.getAverageScore());
+        System.out.println("Did a player break 1000 points?: " + board.break1000());
+
+        System.out.println("\nLeaderboard: ");
+        board.printLeaderboard();
+
+        System.out.println("\nLeaderboard: ");
+        board.addScore("PQR", 150);
+
+        board.printLeaderboard();
+
     }
 }
